62848560c36de133ef138a9381787438182ce0fa
ksolomon7 <kalkidan.solomon2@gmail.com>
2020-10-11 13:55:22 -0400

    adds state to app and author component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import Header from './Header'
 import Author from './Author'
 import './App.css';
 
-function App() {
+class App extends React.Component{
+
+  state={
+    authors: ["Mark Twain", "Michael Jordan", "Babe Ruth"]
+  }
+
+
+  render(){
   return (
     <div className="App">
       <Header title="Welcome to Your Motivational Quotes"/>
-      <Author authorName="Mark Twain"/>
-      <Author authorName= "Michael Jordan"/>
-      <Author authorName="Babe Ruth"/>
+      <hr/>
+      {this.state.authors.map((author,index)=> {
+        return <Author key={index} author={author}/>
+      })}
     </div>
-  );
+  )};
 }
 
 export default App;
--- src/Author.js
+++ src/Author.js
@@ -1,25 +1,43 @@
 import React from 'react'
 
 class Author extends React.Component{
-   
+
+// state holds data within the class or functional component
+// used when something inside of the component needs to changed
+
 state={
    motivated:0 
 }
+
+// setState is a function coming from the React 
+// setstate takes into two arguments-first is the objects 
+// the second is the callback argument- what to do when the state has changed
+
+
    handleClick=(evt)=>{
-       let leveledUpMotivation= this.state.motivated+1
-       this.setState({
-           motivated: leveledUpMotivation
-       })
-       console.log(this.props.authorName)
+    // Although this is not convention-it works
+    //    let leveledUpMotivation= this.state.motivated+1
+    //    this.setState({
+    //        motivated: leveledUpMotivation
+    //    })
+       
+    // Conventional way:functional setState
+    this.setState((state)=>{
+        return{
+            motivated:state.motivated+1
+           
+        }
+    })
    }
-    
-    render(){         
+   
+   
+    render(){        
         return (
-            <li className="container">
-                <img src="https://images.unsplash.com/photo-1528716321680-815a8cdb8cbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=710&q=80" alt="motivational image"/>
-                <p>Author: {this.props.authorName}</p>
+            <div className="container">
+                <img src="https://images.unsplash.com/photo-1528716321680-815a8cdb8cbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=710&q=80" alt="a small board and tree"/>
+                <p>Author: {this.props.author}</p>
                 <button className="motivated-button" onClick={this.handleClick}>I am motivated! <span>{this.state.motivated}</span></button> 
-            </li>
+            </div>
         )
     }
 }
